219dd4283bdb226eb1748e7c6c8a0435c5ac9bce
Matthias Hejlskov <Matthias.Hejlskov@gmail.com>
2025-11-13 16:55:16 +0100

    added testpage to test pages.

---
+++ WebApplication-homework-grupp1/Pages/Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication_homework_grupp1.Pages
+{
+    public class Test2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
