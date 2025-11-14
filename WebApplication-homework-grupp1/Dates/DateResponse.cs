edaab726c359d7523ad099f6c922b874ceae1a2d
Matthias Hejlskov <Matthias.Hejlskov@gmail.com>
2025-11-14 12:53:52 +0100

    ändrade Count till resultCount så det stämmer med API

--- WebApplication-homework-grupp1/Dates/DateResponse.cs
+++ WebApplication-homework-grupp1/Dates/DateResponse.cs
@@ -4,11 +4,8 @@ namespace WebApplication_homework_grupp1.Dates
 {
     public class DateResponse
     {
-        [JsonPropertyName("count")]
+        [JsonPropertyName("resultCount")]
         public int Count { get; set; }
-
-        [JsonPropertyName("message")]
-        public string Message { get; set; } = null!;
 
         [JsonPropertyName("results")]
         public List<DateDto>? Results { get; set; }
