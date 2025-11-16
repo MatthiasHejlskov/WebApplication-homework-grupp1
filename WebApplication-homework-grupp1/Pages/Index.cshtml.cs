451574059e6c0b42ff055559ad5075dd5761fb01
Matthias Hejlskov <Matthias.Hejlskov@gmail.com>
2025-11-16 14:33:16 +0100

    rensade saker som inte fungerade i index och lista

--- WebApplication-homework-grupp1/Pages/Index.cshtml.cs
+++ WebApplication-homework-grupp1/Pages/Index.cshtml.cs
@@ -3,33 +3,7 @@ using WebApplication_homework_grupp1.Data.Services;
 using WebApplication_homework_grupp1.Dates;
 namespace WebApplication_homework_grupp1.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ISkattService _taxService;
-
-        public List<DateDto> WithTax { get; set; } = new();
-        public List<DateDto> WithoutTax { get; set; } = new();
-        public List<int> Months { get; set; } = new();
-        public int TotalTax { get; set; } = 0;
-
-        public IndexModel(ISkattService taxService)
-        {
-            _taxService = taxService;
-        }
-
-        public async Task OnGet()
-        {
-            var allDates = await _taxService.GetDatesAsync();
-
-            var filterService = new DateFilterService();
-            (WithTax, WithoutTax) = filterService.SplitByTax(allDates);
-
-            var monthService = new MonthService();
-            Months = monthService.GetUniqueMonths(allDates);
-
-            TotalTax = WithTax.Sum(d => d.TaxableDay);
-        }
-    }
+   
 }
 
 
